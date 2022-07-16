66401a93c341081770a6c3f69edce3302e066711
Gabriel Yanishi <gabrielyanishi@gmail.com>
2022-07-16 17:57:47 -0300

    Adição do projeto

---
+++ src/main/java/dio/DIODesafio/DioDesafioApplication.java
@@ -0,0 +1,16 @@
+package dio.DIODesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class DioDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesafioApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/dio/DIODesafio/DioDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.DIODesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
